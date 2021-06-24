068d2912f0b35fe9bbc1dc5a2c903f1fe8407279
Eryk <erykcorpo@gmail.co>
2021-06-24 19:09:39 +0200

    Added Base class which hold tests environment configuration.

---
+++ src/main/java/Pages/MainPage/Utils/Base.java
@@ -0,0 +1,26 @@
+package Pages.MainPage.Utils;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public abstract class Base {
+    public static WebDriver webDriver;
+
+    @BeforeAll
+    public static void prepareTestEnvironment() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        webDriver = new ChromeDriver();
+        webDriver.manage().window().maximize();
+        webDriver.get("https://inpost.pl/SzybkieNadania/");
+        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @AfterAll
+    public static void closeTestEnvironment() {
+        webDriver.quit();
+    }
+}
