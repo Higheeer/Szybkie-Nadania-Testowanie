2824d113104a0c5e4ec16f073f9bdb4df786603c
Eryk <erykcorpo@gmail.co>
2021-06-25 19:02:58 +0200

    Added a class responsible for filling out the sender form

---
+++ src/main/java/Pages/MainPage/Components/SenderForm.java
@@ -0,0 +1,50 @@
+package Pages.MainPage.Components;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class SenderForm {
+    private final WebDriver webDriver;
+
+    @FindBy(name = "senderAddress.name")
+    private WebElement nameInput;
+
+    @FindBy(name = "senderAddress.email")
+    private WebElement emailInput;
+
+    @FindBy(name = "senderAddress.phoneNum")
+    private WebElement phoneNumberInput;
+
+    public SenderForm(WebDriver webDriver) {
+        this.webDriver = webDriver;
+        PageFactory.initElements(this.webDriver, this);
+    }
+
+    protected void enterName(String name) {
+        nameInput.sendKeys(name);
+    }
+
+    protected void enterEmail(String email) {
+        emailInput.sendKeys(email);
+    }
+
+    protected void enterPhoneNumber(String phoneNumber) {
+        phoneNumberInput.sendKeys(phoneNumber);
+    }
+
+    public void fill(String name, String email, String phoneNumber) {
+        enterName(name);
+        enterEmail(email);
+        enterPhoneNumber(phoneNumber);
+    }
+
+    enum InvoiceType{
+        COMPANY,
+        INDIVIDUAL,
+        FOREIGN_COMPANY
+    }
+
+}
