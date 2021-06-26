48cbb7cb941903655eb4e8b1cf522496736987f0
Eryk <erykcorpo@gmail.co>
2021-06-26 17:21:37 +0200

    Added a method that waits until the element is clickable

--- src/main/java/Pages/MainPage/Utils/PageAction.java
+++ src/main/java/Pages/MainPage/Utils/PageAction.java
@@ -3,7 +3,10 @@ package Pages.MainPage.Utils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.List;
 
 public class PageAction {
@@ -26,4 +29,15 @@ public class PageAction {
         }
         return false;
     }
+
+    public static void waitUntilClickable(WebElement webElement, WebDriver webDriver) {
+        WebDriverWait waiter = new WebDriverWait(webDriver, Duration.ofSeconds(5));
+        waiter.until(ExpectedConditions.elementToBeClickable(webElement));
+    }
+
+    public static void waitUntilValueInserted(WebElement webElement, String value, WebDriver webDriver) {
+        WebDriverWait waiter = new WebDriverWait(webDriver, Duration.ofSeconds(5));
+        waiter.until(ExpectedConditions.textToBePresentInElementValue(webElement, value));
+    }
+
 }
