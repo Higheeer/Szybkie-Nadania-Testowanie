54c0b7dd0ac7b21439b8cae5d9c51fd2f5407aa5
Eryk <erykcorpo@gmail.co>
2021-06-27 06:29:33 +0200

    Added new functionality. Removed unnecessary arguments from constructor.

--- src/main/java/Pages/MainPage/MainPage.java
+++ src/main/java/Pages/MainPage/MainPage.java
@@ -2,38 +2,64 @@ package Pages.MainPage;
 
 import Pages.MainPage.Components.DeliveryMethod;
 import Pages.MainPage.Components.ParcelSize;
+import Pages.MainPage.Components.RecipientForm.RecipientForm;
+import Pages.MainPage.Components.RecipientForm.RecipientP2H;
+import Pages.MainPage.Components.RecipientForm.RecipientP2P;
+import Pages.MainPage.Components.SenderForm.SenderForm;
 import Pages.MainPage.Utils.PageAction;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-
 public class MainPage {
     private final WebDriver webDriver;
-
     private final DeliveryMethod deliveryMethod;
     private final ParcelSize parcelSize;
 
-    @FindBy(css = "label[for = 'deliveryTypeboxmachine']")
-    private WebElement deliveryP2P;
+    @FindBy(css = "div[id = 'parcelFormButton'] > button")
+    private WebElement submitButton;
 
-    @FindBy(css = "label[for = 'deliveryTypeaddress']")
-    private WebElement deliveryP2D;
+    private RecipientForm recipientForm;
+    private SenderForm senderForm;
 
-    public MainPage(WebDriver webDriver, DeliveryMethod deliveryMethod, ParcelSize parcelSize) {
+    public MainPage(WebDriver webDriver) {
         this.webDriver = webDriver;
         PageFactory.initElements(this.webDriver, this);
-        this.deliveryMethod = deliveryMethod;
-        this.parcelSize = parcelSize;
+
+        this.deliveryMethod = new DeliveryMethod(this.webDriver);
+        this.parcelSize = new ParcelSize(this.webDriver);
     }
 
     public void chooseDeliveryType(DeliveryMethod.Type type) {
         deliveryMethod.choose(type);
+        switch (type) {
+            case P2P: {
+                recipientForm = new RecipientP2P(webDriver);
+                break;
+            }
+            case P2H: {
+                recipientForm = new RecipientP2H(webDriver);
+                break;
+            }
+        }
     }
 
     public void chooseParcelSize(ParcelSize.Size size) {
         parcelSize.choose(size);
+    }
+
+    public void fillRecipientForm(String... values) {
+        recipientForm.fill(values);
+    }
+
+    public void fillSenderForm(String name, String email, String phoneNumber) {
+        senderForm.fill(name, email, phoneNumber);
+    }
+
+    public void wantInvoice(SenderForm.InvoiceType type, String... values) {
+        senderForm.chooseInvoice(type);
+        senderForm.fillInvoice(values);
     }
 
     public boolean acceptTermsAndConditions() {
@@ -48,5 +74,9 @@ public class MainPage {
 
         PageAction.checkCheckBox(name, webDriver);
         return PageAction.isCheckedCheckBox(name, webDriver);
+    }
+
+    public void submit() {
+        submitButton.click();
     }
 }
