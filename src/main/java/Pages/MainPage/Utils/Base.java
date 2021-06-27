56b126b2af0ab8b8df83778db75409eb296dcfbd
Eryk <erykcorpo@gmail.co>
2021-06-27 20:32:23 +0200

    Added better handling of cookies buttons. Implicit wait has been changed to an explicit wait.

--- src/main/java/Pages/MainPage/Utils/Base.java
+++ src/main/java/Pages/MainPage/Utils/Base.java
@@ -4,7 +4,10 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
@@ -16,9 +19,10 @@ public abstract class Base {
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
         webDriver = new ChromeDriver();
         webDriver.manage().window().maximize();
-        webDriver.get("https://inpost.pl/SzybkieNadania/");
-        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
-        webDriver.findElement(By.className("btn-cookie-trigger")).click();
+        webDriver.get("https://test-oneclick-pl.easypack24.net/SzybkieNadania/");
+
+        new WebDriverWait(webDriver, Duration.ofSeconds(5)).until(ExpectedConditions.visibilityOfElementLocated(By.id("onetrust-accept-btn-handler"))).click();
+        new WebDriverWait(webDriver, Duration.ofSeconds(5)).until(ExpectedConditions.visibilityOfElementLocated(By.className("btn-cookie-trigger"))).click();
     }
 
     @AfterAll
