4008ede6fae3a49020218bd0e982ec49b8cd0eb3
Eryk <erykcorpo@gmail.co>
2021-06-30 21:59:02 +0200

    Added automated refresh on problems with page load

--- src/main/java/SzybkieNadania/Utils/Base.java
+++ src/main/java/SzybkieNadania/Utils/Base.java
@@ -2,6 +2,7 @@ package SzybkieNadania.Utils;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -15,8 +16,13 @@ public abstract class Base {
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
         webDriver = new ChromeDriver();
         webDriver.manage().window().maximize();
-        webDriver.get("https://test-oneclick-pl.easypack24.net/SzybkieNadania/");
         webDriver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(15));
+
+        try {
+            webDriver.get("https://test-oneclick-pl.easypack24.net/SzybkieNadania/");
+        } catch (TimeoutException e) {
+            webDriver.navigate().refresh();
+        }
         PageAction.closeCookies(webDriver);
     }
 
