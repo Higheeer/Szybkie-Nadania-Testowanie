65d6c99d361812741241d1fd87b90fbf7e9128c5
Eryk <erykcorpo@gmail.co>
2021-06-30 06:45:58 +0200

    Added better handling of cookies buttons. Added pageLoadTimeout.

--- src/main/java/SzybkieNadania/Utils/Base.java
+++ src/main/java/SzybkieNadania/Utils/Base.java
@@ -1,13 +1,9 @@
-package Pages.MainPage.Utils;
+package SzybkieNadania.Utils;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
@@ -20,9 +16,8 @@ public abstract class Base {
         webDriver = new ChromeDriver();
         webDriver.manage().window().maximize();
         webDriver.get("https://test-oneclick-pl.easypack24.net/SzybkieNadania/");
-
-        new WebDriverWait(webDriver, Duration.ofSeconds(5)).until(ExpectedConditions.visibilityOfElementLocated(By.id("onetrust-accept-btn-handler"))).click();
-        new WebDriverWait(webDriver, Duration.ofSeconds(5)).until(ExpectedConditions.visibilityOfElementLocated(By.className("btn-cookie-trigger"))).click();
+        webDriver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(15));
+        PageAction.closeCookies(webDriver);
     }
 
     @AfterAll
