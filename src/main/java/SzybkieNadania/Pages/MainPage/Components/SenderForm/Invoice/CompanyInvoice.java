75f4249bd7c63f4c082497e73d546435987a49a6
Eryk <erykcorpo@gmail.co>
2021-07-01 17:55:48 +0200

    Added getters and clear methods

--- src/main/java/SzybkieNadania/Pages/MainPage/Components/SenderForm/Invoice/CompanyInvoice.java
+++ src/main/java/SzybkieNadania/Pages/MainPage/Components/SenderForm/Invoice/CompanyInvoice.java
@@ -1,6 +1,7 @@
 package SzybkieNadania.Pages.MainPage.Components.SenderForm.Invoice;
 
 import SzybkieNadania.Utils.PageAction;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -44,19 +45,18 @@ public class CompanyInvoice implements Invoice {
     }
 
     /**
-     * @param values Nip, CompanyName, ZipCode, Town, Street, BuildingNumber, FlatNumber, Email
+     * @param values Nip
      */
     @Override
     public void fill(String... values) {
         if (values.length == 1) {
             enterNip(values[0]);
-
-            new WebDriverWait(webDriver, Duration.ofSeconds(5)).until(ExpectedConditions.attributeToBeNotEmpty(companyNameInput, "value"));
         }
     }
 
     protected void enterNip(String nip) {
         nipInput.sendKeys(nip);
+        new WebDriverWait(webDriver, Duration.ofSeconds(15)).until(ExpectedConditions.attributeToBe(nipInput,"value", nip));
     }
 
     protected void enterCompanyName(String companyName) {
@@ -92,4 +92,100 @@ public class CompanyInvoice implements Invoice {
         emailInput.sendKeys(email);
     }
 
+    protected String getNip() {
+        return nipInput.getAttribute("value");
+    }
+
+    protected String getCompanyName() {
+        return companyNameInput.getAttribute("value");
+    }
+
+    protected String getZipCode() {
+        return zipCodeInput.getAttribute("value");
+    }
+
+    protected String getTown() {
+        return webDriver.findElement(By.xpath("//*[@name='invoice.company.town']/div/div/div[2]/span[2]")).getText();
+    }
+
+    protected String getStreet() {
+        return webDriver.findElement(By.xpath("//*[@name='invoice.company.street']/div/div/div[2]/span[2]")).getText();
+    }
+
+    protected String getBuildingNumber() {
+        return buildingNumberInput.getAttribute("value");
+    }
+
+    protected String getFlatNumber() {
+        return flatNumberInput.getAttribute("value");
+    }
+
+    protected String getEmail() {
+        return emailInput.getAttribute("value");
+    }
+
+    protected boolean getNipError() {
+        return webDriver.findElement(By.xpath("//*[@id='error-invoice.company.nip']/following-sibling::small")).isDisplayed();
+    }
+
+    protected boolean getCompanyNameError() {
+        return webDriver.findElement(By.xpath("//*[@id='error-invoice.company.companyName']/following-sibling::small")).isDisplayed();
+    }
+
+    protected boolean getZipCodeError() {
+        return webDriver.findElement(By.xpath("//*[@id='error-invoice.company.zipCode']/following-sibling::small")).isDisplayed();
+    }
+
+    protected boolean getTownError() {
+        return webDriver.findElement(By.xpath("//*[@id='error-invoice.company.town']/following-sibling::small")).isDisplayed();
+    }
+
+    protected boolean getStreetError() {
+        return webDriver.findElement(By.xpath("//*[@id='error-invoice.company.street']/following-sibling::small")).isDisplayed();
+    }
+
+    protected boolean getBuildingNumberError() {
+        return webDriver.findElement(By.xpath("//*[@id='error-invoice.company.buildingNo']/following-sibling::small")).isDisplayed();
+    }
+
+    protected boolean getFlatNumberError() {
+        return webDriver.findElement(By.xpath("//*[@id='error-invoice.company.flatNo']/following-sibling::small")).isDisplayed();
+    }
+
+    protected boolean getEmailError() {
+        return webDriver.findElement(By.xpath("//*[@id='error-invoice.company.email']/following-sibling::small")).isDisplayed();
+    }
+
+
+    protected void clearNip() {
+        PageAction.waitUntilClickable(nipInput, webDriver).clear();
+    }
+
+    protected void clearCompanyName() {
+        PageAction.waitUntilClickable(companyNameInput, webDriver).clear();
+    }
+
+    protected void clearZipCode() {
+        PageAction.waitUntilClickable(zipCodeInput, webDriver).clear();
+    }
+
+    protected void clearTown() {
+        PageAction.waitUntilClickable(townInput, webDriver).clear();
+    }
+
+    protected void clearStreet() {
+        PageAction.waitUntilClickable(streetInput, webDriver).clear();
+    }
+
+    protected void clearBuildingNumber() {
+        PageAction.waitUntilClickable(buildingNumberInput, webDriver).clear();
+    }
+
+    protected void clearFlatNumber() {
+        PageAction.waitUntilClickable(flatNumberInput, webDriver).clear();
+    }
+
+    protected void clearEmail() {
+        PageAction.waitUntilClickable(emailInput, webDriver).clear();
+    }
 }
